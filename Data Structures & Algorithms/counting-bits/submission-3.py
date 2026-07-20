bf0d99c78606010637448368df6a47ed60fabdfe
Amisha-2905 <143735021+Amisha-2905@users.noreply.github.com>
2026-07-21 00:02:15 +0530

    Add: counting-bits - submission-3

---
+++ Data Structures & Algorithms/counting-bits/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
+
+        stack = [0, 1]
+        idx = 2
+        curr_p = 0
+        len_s = 2
+
+        while idx <= n:
+            if curr_p == len_s:
+                len_s *= 2
+                curr_p = 0
+
+            stack.append(stack[curr_p] + 1)
+            curr_p += 1
+            idx += 1
+
+        return stack
